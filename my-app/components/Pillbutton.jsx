263c2e88b7c48586fcb33882b3d281e43a7decf3
Michael Cuccia <michael.cuccia@egplusww.com>
2024-04-23 10:51:22 -0400

    color toggle context is working

--- my-app/components/Pillbutton.jsx
+++ my-app/components/Pillbutton.jsx
@@ -1,6 +1,8 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import ThemeContext from '@/app/context/ThemeContext';
 import styled from 'styled-components';
+import { lightMode, darkMode } from '../constants/palette';
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -10,6 +12,7 @@ const ButtonContainer = styled.div`
   height: 40px; 
   border-radius: 20px; 
   background-color: #f0f0f0; 
+  //background-color: ${({ theme }) => (theme === 'lightMode' ? lightMode.Background : darkMode.Background)};
   position: relative; 
 `;
 
@@ -26,7 +29,8 @@ const Button = styled.button`
 const Circle = styled.div`
   width: 40px; 
   height: 40px; 
-  background-color: #007bff; 
+  //background-color: #007bff; 
+  background-color: ${({ theme }) => (theme === 'lightMode' ? lightMode.Background : darkMode.Background)};
   border-radius: 50%; 
   position: absolute;
   left: ${({ toggled }) => (toggled ? 'calc(100% - 40px)' : '0')}; 
@@ -38,15 +42,27 @@ const Circle = styled.div`
 const PillButton = () => {
 
   const [toggled, setToggled] = useState(false);
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  console.log('pillbutton ' + theme)
 
   const handleToggle = () => {
     setToggled(!toggled);    
+    if (theme === 'lightMode') {
+      setTheme('darkMode');
+    } else if (theme === 'darkMode') {
+      setTheme('lightMode');
+    }
   };
+
+  useEffect(() => {
+
+  },[handleToggle])
 
   return (
     <ButtonContainer>
       <Button onClick={handleToggle}>
-        <Circle toggled={toggled} />
+        <Circle toggled={toggled} theme={theme}/>
       </Button>
     </ButtonContainer>
   );
