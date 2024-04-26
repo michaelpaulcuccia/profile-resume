553e0d7b5be7bdd724f85bb62ce99b7078be00dc
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-04-26 18:25:13 -0400

    adds font, updates page layout height

--- my-app/components/Hero.jsx
+++ my-app/components/Hero.jsx
@@ -1,100 +1,96 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import styled from 'styled-components';
-import HeroImage from '../public/assets/mpc.png';
-import GitHub from '../public/assets/GitHub.png';
-//import LinkedIn from '../public/assets/LinkedIn.png';
-import LinkedIn from '../public/assets/LinkedIn.jpeg';
-import { mobileBreakpoint } from '../constants/breakpoint';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+import HeroImage from "../public/assets/mpc.png";
+import GitHub from "../public/assets/GitHub.png";
+import LinkedIn from "../public/assets/LinkedIn.jpeg";
+import { mobileBreakpoint } from "../constants/breakpoint";
 
 const Root = styled.div`
-    display: flex;
-    padding: 0 60px;
-    letter-spacing: 1.5px;
+  display: flex;
+  padding: 0 60px;
+  letter-spacing: 1.5px;
 
-    h1 {
-        font-size: 42px;
-    }
+  h1 {
+    font-size: 42px;
+  }
 
-    h2 {
-        font-size: 34px;
-    }
+  h2 {
+    font-size: 34px;
+  }
 
-    @media (max-width: ${mobileBreakpoint}) {
-        flex-direction: column;
-        align-items: center;
-    }
+  @media (max-width: ${mobileBreakpoint}) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const ContentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 0 auto;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  text-align: center;
 `;
 
 const LinksFlex = styled.div`
-    display: flex;
-    align-items: center;
-    margin-top: 16px;
+  display: flex;
+  align-items: center;
+  margin-top: 16px;
 `;
 
 const IconWithLinkContainer = styled.div`
-    margin-left: 6px;
-    margin-right: 6px;
+  margin-left: 6px;
+  margin-right: 6px;
 `;
 
 export default function Hero() {
   return (
     <Root>
-        <Image 
-            src={HeroImage}
-            width={275}
-            height={275}
-            alt="Michael-Paul Cuccia"
-            style={{ borderRadius: '12px' }}
-        />
-        <ContentContainer>
-            <h1>Michael-Paul Cuccia</h1>
-            <h2>Full Stack Developer</h2>
-            <h4>Austin, Texas</h4>
-            <br/>
-            <h4>Former Paralegal Specialist (US Department of Justice)</h4>
-            <h4>United States Army Veteran</h4>
-            <LinksFlex>
-                <IconWithLinkContainer>
-                    <Link
-                        href='https://github.com/michaelpaulcuccia'
-                        target='_blank'
-                    >
-                        <Image
-                            src={GitHub}
-                            width={40}
-                            height={40}
-                            alt="GitHub"
-                            style={{ borderRadius: '12px' }}
-                        />
-                    </Link>
-                </IconWithLinkContainer>
-                <IconWithLinkContainer>
-                    <Link
-                        href='https://www.linkedin.com/in/michael-paul-cuccia-882607275/'
-                        target='_blank'
-                    >
-                        <Image
-                            src={LinkedIn}
-                            width={40}
-                            height={40}
-                            alt="LinkedIn"
-                            style={{ borderRadius: '12px' }}
-                        />
-                    </Link>
-                </IconWithLinkContainer>
-            </LinksFlex>
-        </ContentContainer>
+      <Image
+        src={HeroImage}
+        width={275}
+        height={275}
+        alt="Michael-Paul Cuccia"
+        style={{ borderRadius: "12px" }}
+      />
+      <ContentContainer>
+        <h1>Michael-Paul Cuccia</h1>
+        <h2>Full Stack Developer</h2>
+        <h4>Austin, Texas</h4>
+        <br />
+        <h4>Former Paralegal Specialist (US Department of Justice)</h4>
+        <h4>United States Army Veteran</h4>
+        <LinksFlex>
+          <IconWithLinkContainer>
+            <Link href="https://github.com/michaelpaulcuccia" target="_blank">
+              <Image
+                src={GitHub}
+                width={40}
+                height={40}
+                alt="GitHub"
+                style={{ borderRadius: "12px" }}
+              />
+            </Link>
+          </IconWithLinkContainer>
+          <IconWithLinkContainer>
+            <Link
+              href="https://www.linkedin.com/in/michael-paul-cuccia-882607275/"
+              target="_blank"
+            >
+              <Image
+                src={LinkedIn}
+                width={40}
+                height={40}
+                alt="LinkedIn"
+                style={{ borderRadius: "12px" }}
+              />
+            </Link>
+          </IconWithLinkContainer>
+        </LinksFlex>
+      </ContentContainer>
     </Root>
-  )
+  );
 }
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,26 +1,31 @@
 "use client";
-import React, { useContext } from 'react';
-import ThemeContext from './context/ThemeContext';
-import styled from 'styled-components';
-import { lightMode, darkMode } from '../../constants/palette';
-import PageSection from '../../components/PageSection';
-import Pillbutton from '../../components/Pillbutton';
-import Hero from '../../components/Hero';
-import NavBar from '../../components/NavBar';
+import React, { useContext } from "react";
+import ThemeContext from "./context/ThemeContext";
+import styled from "styled-components";
+import { Inconsolata } from "next/font/google";
+import { lightMode, darkMode } from "../../constants/palette";
+import PageSection from "../../components/PageSection";
+import Pillbutton from "../../components/Pillbutton";
+import Hero from "../../components/Hero";
+import NavBar from "../../components/NavBar";
+
+const inconsolata = Inconsolata({ weight: ["400", "800"], subsets: ["latin"] });
 
 const Root = styled.div`
-  color: ${({ theme }) => (theme === 'lightMode' ? lightMode.Text : darkMode.Text)};
-  background-color: ${({ theme }) => (theme === 'lightMode' ? lightMode.Background : darkMode.Background)};;
+  color: ${({ theme }) =>
+    theme === "lightMode" ? lightMode.Text : darkMode.Text};
+  background-color: ${({ theme }) =>
+    theme === "lightMode" ? lightMode.Background : darkMode.Background};
+  height: 100vh;
 `;
 
 export default function page() {
-
   const { theme } = useContext(ThemeContext);
 
   return (
-    <Root theme={theme}>
+    <Root theme={theme} className={inconsolata.className}>
       <PageSection>
-        <Pillbutton/>
+        <Pillbutton />
       </PageSection>
       <PageSection>
         <Hero />
@@ -29,5 +34,5 @@ export default function page() {
         <NavBar />
       </PageSection>
     </Root>
-  )
+  );
 }
