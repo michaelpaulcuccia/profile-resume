a194159193009e4346665335e60dfbc0c66d5cd7
Michael Cuccia <michael.cuccia@egplusww.com>
2024-04-26 09:20:13 -0400

    adds linkedin and github

--- my-app/components/Hero.jsx
+++ my-app/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 import HeroImage from '../public/assets/mpc.png';
+import GitHub from '../public/assets/GitHub.png';
+//import LinkedIn from '../public/assets/LinkedIn.png';
+import LinkedIn from '../public/assets/LinkedIn.jpeg';
 import { mobileBreakpoint } from '../constants/breakpoint';
 
 const Root = styled.div`
@@ -32,6 +36,17 @@ const ContentContainer = styled.div`
     text-align: center;
 `;
 
+const LinksFlex = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 16px;
+`;
+
+const IconWithLinkContainer = styled.div`
+    margin-left: 6px;
+    margin-right: 6px;
+`;
+
 export default function Hero() {
   return (
     <Root>
@@ -49,6 +64,36 @@ export default function Hero() {
             <br/>
             <h4>Former Paralegal Specialist (US Department of Justice)</h4>
             <h4>United States Army Veteran</h4>
+            <LinksFlex>
+                <IconWithLinkContainer>
+                    <Link
+                        href='https://github.com/michaelpaulcuccia'
+                        target='_blank'
+                    >
+                        <Image
+                            src={GitHub}
+                            width={40}
+                            height={40}
+                            alt="GitHub"
+                            style={{ borderRadius: '12px' }}
+                        />
+                    </Link>
+                </IconWithLinkContainer>
+                <IconWithLinkContainer>
+                    <Link
+                        href='https://www.linkedin.com/in/michael-paul-cuccia-882607275/'
+                        target='_blank'
+                    >
+                        <Image
+                            src={LinkedIn}
+                            width={40}
+                            height={40}
+                            alt="LinkedIn"
+                            style={{ borderRadius: '12px' }}
+                        />
+                    </Link>
+                </IconWithLinkContainer>
+            </LinksFlex>
         </ContentContainer>
     </Root>
   )
